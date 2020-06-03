47085f1294e5ca5b831c2a1f5e7757d3efa10aab
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-03 17:27:13 +0530

    Echo Command Complete

--- src/main/scala/com/vinay/demofilesystem/commands/Command.scala
+++ src/main/scala/com/vinay/demofilesystem/commands/Command.scala
@@ -16,14 +16,11 @@ object Command {
   val TOUCH = "touch"
   val CD = "cd"
   val RM = "rm"
+  val ECHO = "echo"
 
   def emptyCommand : Command = (state: State) => state
 
-  def incompleteCommand(name: String) : Command = new Command {
-    override def apply(state: State): State =
-      state.setMessage(name + " incomplete command!")
-  }
-
+  def incompleteCommand(name: String) : Command = (state: State) => state.setMessage(name + " incomplete command!")
 
 
   def from(input: String) : Command = {
@@ -45,6 +42,9 @@ object Command {
     } else if(RM.equals(tokens(0))){
       if(tokens.length < 2) incompleteCommand(RM)
       else new Rm(tokens(1))
+    } else if (ECHO.equals(tokens(0))){
+      if(tokens.length < 2) incompleteCommand(ECHO)
+      else new Echo(tokens.tail)
     }
     else new UnknownCommand
   }
---
+++ src/main/scala/com/vinay/demofilesystem/commands/Echo.scala
@@ -0,0 +1,92 @@
+package com.vinay.demofilesystem.commands
+import com.vinay.demofilesystem.files.{Directory, File}
+import com.vinay.demofilesystem.filesystem.State
+
+import scala.annotation.tailrec
+
+class Echo(args: Array[String]) extends Command {
+
+  override def apply(state: State): State = {
+    /*
+      if no args, state
+      else if just one arg, print to console
+      else if multiple args
+      {
+        operator = next to last argument
+        if >
+         echo to a file (may create a file uf not there)
+        if  >>
+           append to a file
+        else
+          just echo everything to console
+      }
+     */
+    if (args.isEmpty) state
+    else if (args.length == 1) state.setMessage(args(0))
+    else {
+      val operator = args(args.length-2)
+      val filename = args(args.length-1)
+      val contents = createContent(args, args.length-2)
+      if(">>".equals(operator))
+        doEcho(state, contents, filename, append = true)
+      else if(">".equals(operator))
+        doEcho(state, contents, filename, append = false)
+      else
+        state.setMessage(createContent(args, args.length))
+    }
+  }
+
+  def getRootAfterEcho(currentDirectory: Directory, path: List[String], contents: String,append: Boolean): Directory = {
+    /*
+      if path is e,pty, then fail (currentDirectory)
+      else if no more things to explore = path.tail.isEmpty
+        find the file to create/add content to
+        if file not found, create file
+        else if the entry is actually a directory, then fai
+        else
+          replace or append contents to the file
+          replace the entry with the filename with the NEW file
+       else
+          find
+
+     */
+    if (path.isEmpty) currentDirectory
+    else if (path.tail.isEmpty) {
+      val dirEntry = currentDirectory.findEntry(path.head)
+        if(dirEntry == null)
+          currentDirectory.addEntry(new File(currentDirectory.path, path.head, contents))
+        else if (dirEntry.isDirectory) currentDirectory
+        else if(append)currentDirectory.replaceEntry(path.head, dirEntry.asFile.appendContents(contents))
+        else currentDirectory.replaceEntry(path.head, dirEntry.asFile.setContents(contents))
+
+    } else{
+      val nextDirectory = currentDirectory.findEntry(path.head).asDirectory
+      val newNextDirectory = getRootAfterEcho(nextDirectory, path.tail, contents, append)
+      if(newNextDirectory == nextDirectory) currentDirectory
+      else currentDirectory.replaceEntry(path.head,newNextDirectory)
+    }
+  }
+
+  def doEcho(state: State, contents: String, fileName: String, append: Boolean): State = {
+    if(fileName.contains(Directory.SEPARATOR))
+      state.setMessage("Echo: filename must not contain separators")
+    else{
+      val newRoot: Directory = getRootAfterEcho(state.root, state.wd.getAllFoldersInPath :+ fileName, contents, append)
+      if(newRoot == state.root)
+        state.setMessage(s"$fileName : no such file")
+      else
+        State(newRoot, newRoot.findDescendant(state.wd.getAllFoldersInPath))
+    }
+  }
+
+  // topIndex NON-INCLUSIVE!
+  def createContent(args: Array[String], topIndex: Int): String = {
+    @tailrec
+    def createContentHelper( currentIndex: Int, accumulator: String): String = {
+      if(currentIndex>= topIndex) accumulator
+      else createContentHelper(currentIndex+1, accumulator+" "+args(currentIndex))
+    }
+    createContentHelper(0, "")
+  }
+
+}
